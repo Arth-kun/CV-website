8b9db777ece361faf48662a5b231b657985dfdfe
Arth-kun <arthur.couge@gmail.com>
2015-12-02 00:20:32 +0100

    CV

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+
+	<title>Arthur Cougé</title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bootstrap-theme.min.css">
+	<link rel="stylesheet" href="styles/style.css">
+
+	<script src="scripts/jquery-2.1.4.js"></script>
+	<script src="scripts/script.js"></script>
+	<!--<script src="bootstrap/js/bootstrap.min.js"></script>-->
+
+</head>
+<body>
+	<div class="container">
+
+		<header class="row">
+			<!--Header-->
+			<div class="col-lg-4">
+				<!--logo-->
+			</div>
+
+			<div class="col-lg-5">
+				<!--Titre-->
+			</div>
+
+			<div class="col-lg-3">
+				<!--Photo-->
+			</div>
+
+		</header>
+
+		<nav class="row">
+			<!--Navbar-->
+			<div class="col-lg-5">
+				<!--Compétence en mode déroulant-->
+			</div>
+
+			<div class="col-lg-5">
+				<!--Coordonées-->
+			</div>
+
+			<div class="col-lg-2">
+				<!--ABOUT ME A METTRE ICI-->
+			</div>
+
+		</nav>
+
+		<section class="row">
+			<!--Experiences-->
+
+			<aside class="col-lg-2">
+				<!--BAR LATERAL-->
+			</aside>
+
+			<article class="col-lg-10">
+				<!--TEXTE-->
+			</article>
+
+		</section>
+
+		<section class="row">
+			<div class="col-lg-12">
+				<!--Formations-->
+				<div class="row">
+
+					<article class="col-lg-5">
+						<!--EN COURS-->
+					</article>
+
+					<div class="col-lg-2">
+						<!--BAR  MILLIEU-->
+					</div>
+
+					<article class="col-lg-5">
+						<!--ACQUIS-->
+					</article>
+
+				</div>	
+			</div>
+		</section>
+
+		<footer class="row">
+
+			<article class="col-lg-6">
+				<!--PROFIL OU PAS PAS SUR-->
+			</article>
+
+			<article class="col-lg-6">
+				<!--CONTACT RS-->
+			</article>
+
+		</footer>
+
+	</div>
+</body>
+</html>
